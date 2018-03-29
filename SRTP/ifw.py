eeec3784bf8292225fac7a042cb454bc49c4f3b6
Sylar <1020264418@qq.com>
2018-03-29 14:02:24 +0800

    111

---
+++ SRTP/ifw.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+import re
+import shutil
+import os
+
+filepath = 'C:\\Users\\Sylar\\Desktop\\SRTP\\demo1\\'
+path1 = 'C:\\Users\\Sylar\\Desktop\\SRTP\\提取\\是否\\车牌\\'
+path2 = 'C:\\Users\\Sylar\\Desktop\\SRTP\\提取\\是否\\驾驶证\\'
+path3 = 'C:\\Users\\Sylar\\Desktop\\SRTP\\提取\\是否\\检查\\'
+num = 1
+total1 = 0
+total2 = 0
+total3 = 0
+
+
+def ifw():
+    global num
+    while num <= 100:
+        name = "%d" % num
+        fileName = filepath + str(name) + ".txt"
+        source = open(fileName, 'r')
+        sentence = source.read()
+        res = re.findall(r'伪造车牌|变造车牌|伪造驾驶证|变造驾驶证|逃避检查|拒绝检查|阻碍检查', sentence)
+        for item in res:
+            if (item == '伪造车牌') | (item == '变造车牌'):
+                resname1 = path1 + str(name) + ".txt"
+                if os.path.exists(resname1):
+                    os.remove(resname1)
+                shutil.copyfile(fileName, resname1)
+            if (item == '伪造驾驶证') | (item == '变造驾驶证'):
+                resname2 = path2 + str(name) + ".txt"
+                if os.path.exists(resname2):
+                    os.remove(resname2)
+                shutil.copyfile(fileName, resname2)
+            if (item == '逃避检查') | (item == '拒绝检查') | (item == '阻碍检查'):
+                resname3 = path3 + str(name) + ".txt"
+                if os.path.exists(resname3):
+                    os.remove(resname3)
+                shutil.copyfile(fileName, resname3)
+        num = num + 1
+
+
+if __name__ == '__main__':
+    ifw()
+    for root, dirs, files in os.walk(path1):  # 遍历统计
+        for each in files:
+            total1 += 1  # 统计文件夹下文件个数
+    for root, dirs, files in os.walk(path2):  # 遍历统计
+        for each in files:
+            total2 += 1  # 统计文件夹下文件个数
+    for root, dirs, files in os.walk(path3):  # 遍历统计
+        for each in files:
+            total3 += 1  # 统计文件夹下文件个数
+    print("车牌：", total1)
+    print("驾驶证：", total2)
+    print("检查：", total3)
