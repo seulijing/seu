8c92f70e64cb5b54db9cb0ee386d34f633d341aa
Sylar <1020264418@qq.com>
2018-03-29 14:13:43 +0800

    111

---
+++ SRTP/loc.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import re
+import os
+filepath = 'C:\\Users\\Sylar\\Desktop\\SRTP\\demo1\\'
+path = 'C:\\Users\\Sylar\\Desktop\\SRTP\\提取\\地点\\'
+num = 1
+while num <= 100:
+    name = "%d" % num
+    fileName = filepath + str(name) + ".txt"
+    if os.path.exists(fileName):
+        source = open(fileName, 'r')
+        resName = path + str(name) + ".txt"
+        if os.path.exists(resName):
+            os.remove(resName)
+        result = open(resName, 'w')
+        sentence = source.read()
+        res = re.findall(r'路口|街道|高速公路|城市干道|隧道|桥|交叉口', sentence)
+        list1 = sorted(set(res), key=res.index)
+        for item in list1:
+            result.write(item + ' ')
+        source.close()
+        result.close()
+        print(num, list1)
+    num = num + 1
